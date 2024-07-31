4c6b7a2f66064883f84efbfda4bfa73798a9134f
shariard58 <shariard58@gmail.com>
2024-07-31 13:23:22 +0600

    reducer function and store created

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 const initialState = {
   value: 0,
 };
+
+// now I will create the reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+// create store
+const store = Redux.createStore(counterReducer);
+
+// button Click Listener
